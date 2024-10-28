5f42531ad6e728c9a80e8fa17c4580d74eca802e
gaus07 <gausnoori07@gmail.com>
2024-10-28 18:32:24 +0530

    Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
-  return(
-    <div className="">
-      Hello World
-    </div>
-  )
+  return <div className="">Hello World 2</div>;
 }
